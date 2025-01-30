a62f6e27a3163050deeaa338d65a918aa0adb9d5
luddthree <luddetv@gmail.com>
2025-01-30 16:05:48 +0100

    Remove test route from API and clean up whitespace in routes file

--- routes/api.php
+++ routes/api.php
@@ -6,11 +6,6 @@ use App\Http\Controllers\TaskController;
 Route::post('/register', [UserController::class, 'register']);
 Route::post('/login', [UserController::class, 'login']);
 
-
-Route::get('/test', function () {
-    return response()->json(['message' => 'Test route works']);
-});
-
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', [UserController::class, 'getAuthenticatedUser']);
 
@@ -19,7 +14,7 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::put('/tasks/{task}/complete', [TaskController::class, 'markComplete']);
     Route::delete('/tasks/{id}', [TaskController::class, 'deleteTask']);
     Route::put('/tasks/{id}', [TaskController::class, 'updateTask']);
-    
+
     Route::put('/user/profile', [UserController::class, 'updateProfile']);
     Route::put('/user/password', [UserController::class, 'updatePassword']);
 });
